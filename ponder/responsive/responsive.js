674e01540c6a852794e274d3e437c03357378302
Rollick-Torrador <dmoore2691@byui.edu>
2026-01-31 16:18:43 -0700

    changes

---
+++ ponder/responsive/responsive.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('.menu-btn');
+const nav = document.querySelector('nav');
+
+btn.addEventListener('click', (toggleMenu) => {
+    nav.classList.toggle('hide');
+    btn.classList.toggle('change');
+});
